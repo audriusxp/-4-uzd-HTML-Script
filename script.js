33fb3b404d56a4a29961778f023fcbdbd1bea43c
audriusxp <palm@inbox.lt>
2022-06-21 15:03:34 +0300

    teorsikai pabaigta, bet dar abejoju

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 window.onload = function(){
-    
+    let zodis1Laukelis = document.getElementById("zodis1");
+    console.log(zodis1Laukelis);
+
+    let zodis2Laukelis = document.getElementById("zodis2");
+    console.log(zodis2Laukelis);
+
+    let mygtukas = document.getElementById("skaiciavimoMygtukas");
+    console.log(mygtukas);
+
+    let pastraipa = document.getElementById("rezultatoPastraipa");
+
+    mygtukas.onclick = function() {
+        let zodis1 = zodis1Laukelis.value;
+        console.log(zodis1);
+
+        let zodis2 = zodis2Laukelis.value;
+        console.log(zodis2);
+
+        console.log(zodziuPalyginimas(zodis1, zodis2));
+
+        pastraipa.innerHTML = "Zodziu palyginimas: " + zodziuPalyginimas(zodis1,zodis2);
+    }
 }
+function zodziuPalyginimas(zodis1, zodis2) {
+    if (zodis1.length === zodis2.length) {
+        return zodis1 ,zodis2;
+    }
+    else if (zodis1.length > zodis2.length) {
+        return zodis1;
+    }
+    else {
+        return zodis2;
+    }
+    }
